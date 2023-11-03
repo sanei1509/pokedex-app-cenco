8678e2c69c84042349b2c3011420a7458c879327
Santiago Neira <santiago.neira@cencosud.com>
2023-11-03 14:59:18 -0300

    Initial Commit

---
+++ pokedex-app-cenco/pokedex_app_cencoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_app_cencoApp.swift
+//  pokedex-app-cenco
+//
+//  Created by Santiago Neira on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_app_cencoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
