ec0501efba2c4a73e1421fc0dfb9260e733cf362
Santiago Neira <santiago.neira@cencosud.com>
2023-11-21 22:39:34 -0300

    hot fix: order de Pokemon model

--- pokedex-app-cenco/Model/Pokemon.swift
+++ pokedex-app-cenco/Model/Pokemon.swift
@@ -4,10 +4,6 @@
 //
 //  Created by Santiago Neira on 6/11/23.
 //
-
-
-
-
 import Foundation
 import SwiftUI
 
@@ -68,34 +64,7 @@ struct PokemonResponse: Codable {
     let results: [Pokemon]
 }
 
-//struct Pokemon: Codable {
-//    var name: String
-//    var url: String
-//    var id: Int? {
-//        return Int(url.split(separator: "/").last?.description ?? "0")
-//    }
-//    
-//    var imageUrl: URL? {
-//        if let id = self.id {
-//            return URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/\(id).png")
-//        }
-//        return nil
-//    }
-//    
-//}
-
-
-//let PokemonList:[Pokemon2] = [
-//    .init(id: 1, name: "Ivysaur", imageUrl: "", type: "poison", description: "", height: 5, weight: 5),
-//    .init(id: 2, name: "Pikachu", imageUrl: "", type: "electric", description: "", height: 3, weight: 10),
-//    .init(id: 3, name: "Squirtle", imageUrl: "", type: "water", description: "", height: 5, weight: 5),
-//    .init(id: 4, name: "Charmander", imageUrl: "", type: "fire", description: "", height: 10, weight: 15),
-//    .init(id: 5, name: "Ivysaur", imageUrl: "", type: "psychic", description: "", height: 5, weight: 5),
-//    .init(id: 6, name: "Pikachu", imageUrl: "", type: "fairy", description: "", height: 3, weight: 10),
-//    .init(id: 7, name: "Squirtle", imageUrl: "", type: "normal", description: "", height: 5, weight: 5),
-//    .init(id: 8, name: "Charmander", imageUrl: "", type: "flying", description: "", height: 10, weight: 15)
-//]
-
+// Codigo para poder poner colores hexadecimales
 extension Color {
     init(hex: String) {
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -130,13 +99,6 @@ func backgroundColor(forType type:String)-> Color{
     }
 }
 
-
-struct PokemonPage: Codable {
-    let count: Int
-    let next: String
-    let results: [Pokemon]
-}
-
 struct Pokemon: Codable{
     var id: Int? {
         return Int(url.split(separator: "/").last?.description ?? "0")
@@ -146,12 +108,6 @@ struct Pokemon: Codable{
 //    let types: [String]
     static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
 }
-
-
-//struct TypeElement: Codable {
-//    let type: String
-//}
-
 
 struct DetailPokemon: Codable {
     let id: Int
